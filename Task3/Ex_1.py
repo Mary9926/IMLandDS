9ca92a68f2912278c1bdf1cdebb38b2ed9582d1d
Mary9926 <maria.gryglewska51@gmail.com>
2022-03-29 23:06:17 +0200

    task 1

--- Task3/Ex_1.py
+++ Task3/Ex_1.py
@@ -1,4 +1,3 @@
-from traceback import print_tb
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('yob2009.txt', header=None, names=['name','sex','count'])
@@ -13,13 +12,34 @@ df9 = pd.read_csv('yob2017.txt', header=None, names=['name','sex','count'])
 df10 = pd.read_csv('yob2018.txt', header=None, names=['name','sex','count'])
 sum_of_dfs = [df, df2, df3, df4, df5, df6, df7, df8, df9, df10]
 all_dfs = pd.concat(sum_of_dfs)
-all_dfs2 = all_dfs.groupby(['name', 'sex'])['count'].sum()
-# print(all_dfs2)
 
-girls_names = all_dfs2[(all_dfs2['name']) & (all_dfs2["sex"] == "M") & (all_dfs2["count"])]
-# girls_names = pd.DataFrame(all_dfs2)
-# girls_names2 = girls_names.drop
-# print(girls_names)
-fig = all_dfs2.plot(x = 'name' ,kind = 'line', title = 'chuj', xlabel="Names", ylabel="Count")
-# fig.axvline(x = 1948) #vertical line on x = 1963 - that's when the vaccine was introduced in Cali
+all_dfs = pd.DataFrame(all_dfs)
+#print(all_dfs)
+all_dfs2 = all_dfs.groupby(['name', 'sex'], as_index=False)['count'].sum()
+
+all_dfs2 = pd.DataFrame(all_dfs2)
+#print(all_dfs2)
+#all_dfs2.to_csv('all_dfs2.csv', sep=";", index=False, columns=['name', 'sex', 'count'])
+
+
+girls_names = all_dfs2.loc[(all_dfs2['sex'] == 'F')]
+girls_names = pd.DataFrame(girls_names)
+print(girls_names)
+
+boys_names = all_dfs2.loc[(all_dfs2['sex'] == 'M')]
+boys_names = pd.DataFrame(boys_names)
+print(boys_names)
+
+
+top_3Girls = girls_names.nlargest(3, 'count')
+top_3Boys = boys_names.nlargest(3, 'count')
+
+print("top_3Girls")
+print(top_3Girls)
+print("top_3Girls")
+print(top_3Boys)
+
+fig = top_3Girls.plot(x = 'name' ,kind = 'bar', title = 'top 3 girls names', xlabel="Names", ylabel="Count")
+plt.show()
+fig = top_3Boys.plot(x = 'name' ,kind = 'bar', title = 'top 3 boys names', xlabel="Names", ylabel="Count")
 plt.show()
